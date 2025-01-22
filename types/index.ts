ef4c96118dfdd82b469de9d1b8f4477f4def3909
Chitralekhayaduvanshi <102354726+Chitralekhayaduvanshi@users.noreply.github.com>
2025-01-22 16:07:08 +0530

    Revert "User & Team Management"
    This reverts commit 588ad8b939393354798d45e58757329f1bb33973.

--- types/index.ts
+++ types/index.ts
@@ -24,23 +24,4 @@ export interface MongoIncident extends BaseIncident {
 export interface Incident extends BaseIncident {
   _id: string
   id?: string
-}
-
-export interface User {
-  _id: string
-  email: string
-  name: string
-  role: "admin" | "member"
-  teamId?: string
-  createdAt: Date
-  updatedAt?: Date
-}
-
-export interface Team {
-  _id: string
-  name: string
-  description?: string
-  members: string[] // User IDs
-  createdAt: Date
-  updatedAt?: Date
 } 
